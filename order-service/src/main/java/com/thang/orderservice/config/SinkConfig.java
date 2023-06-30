5e7e07ac9c01e48f21b14ec0792fa96ac45b264b
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-06-30 16:41:04 +0700

    SSE - change sink to product service

--- order-service/src/main/java/com/thang/orderservice/config/SinkConfig.java
+++
@@ -1,2 +0,0 @@
-package com.thang.orderservice.config;public class SinkConfig {
-}
---
+++ product-service/src/main/java/com/thang/productservice/config/SinkConfig.java
@@ -0,0 +1,22 @@
+package com.thang.productservice.config;
+
+
+import com.thang.productservice.dto.ProductDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+@Configuration
+public class SinkConfig {
+
+    @Bean
+    public Sinks.Many<Object> sink() {
+        return Sinks.many().replay().limit(1);
+    }
+
+    @Bean
+    public Flux<ProductDto> productBroadcast(Sinks.Many<ProductDto> sink) {
+        return sink.asFlux();
+    }
+}
